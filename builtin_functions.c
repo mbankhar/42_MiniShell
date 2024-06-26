c18d78dda1a7f58d04bc81028901a9b5132220ac
Ahmed Mohamed <58007225+0xcro3dile@users.noreply.github.com>
2024-06-26 16:34:13 +0200

    to be editied

--- builtin_functions.c
+++ builtin_functions.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_functions.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbankhar <mbankhar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amohame2 <amohame2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 18:45:25 by mbankhar          #+#    #+#             */
-/*   Updated: 2024/06/26 14:41:11 by mbankhar         ###   ########.fr       */
+/*   Updated: 2024/06/26 16:15:02 by amohame2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,35 @@ void	change_directory(char *path)
 	}
 }
 
+void execute_export(const char *var, const char *value) {
+    // Simulate setting an environment variable by writing to a file
+    char filename[256];
+    snprintf(filename, sizeof(filename), "%s.env", var);
+
+    // Check if the file can be accessed
+    if (access(filename, F_OK) != -1) {
+        // File exists, write the value to the file
+        FILE *file = fopen(filename, "w");
+        if (file != NULL) {
+            fprintf(file, "%s", value);
+            fclose(file);
+            printf("Exported %s to %s with value: %s\n", var, filename, value);
+        } else {
+            perror("fopen");
+        }
+    } else {
+        // File does not exist, create and write to the file
+        FILE *file = fopen(filename, "w");
+        if (file != NULL) {
+            fprintf(file, "%s", value);
+            fclose(file);
+            printf("Exported %s to %s with value: %s\n", var, filename, value);
+        } else {
+            perror("fopen");
+        }
+    }
+}
+
 void	execute_echo(char *args[])
 {
 	int	i;
@@ -49,21 +78,21 @@ void	execute_echo(char *args[])
 	printf("\n");
 }
 
-void	execute_export(char *var, char *value)
+void	execute_unset(char *var)
 {
-	if (setenv(var, value, 1) == -1)
+	if (access(var, F_OK) != -1)
 	{
-		perror("setenv");
+		if (unlink(var) == -1)
+		{
+			perror("unlink");
+		}
+	}
+	else
+	{
+		perror("access");
 	}
 }
 
-void	execute_unset(char *var)
-{
-	if (unsetenv(var) == -1)
-	{
-		perror("unsetenv");
-	}
-}
 
 void	print_env()
 {
